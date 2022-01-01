d8c18c800357bb81a63ee0aa05328a939178fac3
Craig Young <youngcraig@gmail.com>
2022-01-01 14:01:18 -0700

    Added configuration constants

---
+++ src/QuotebotData/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotebot.Data
+{
+    internal static class Constants
+    {
+        public const string CosmosUrlConfigurationKey = "CosmosEndpointUrl";
+        public const string CosmosAuthorizationConfigurationKey = "CosmosAuthorization";
+    }
+}

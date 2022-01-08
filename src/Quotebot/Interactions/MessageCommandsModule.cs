d3415ecf4fc4f80d34a67ab734fdf18113fd3d44
Craig Young <youngcraig@gmail.com>
2022-01-08 12:25:56 -0700

    Significantly better handling for searching quotes

--- src/Quotebot/Interactions/MessageCommandsModule.cs
+++ src/Quotebot/Interactions/MessageCommandsModule.cs
@@ -18,13 +18,21 @@ namespace Quotebot.Interactions
         {
             if (rawMessage is not SocketUserMessage message)
                 return;
+
+            if(message.Author.IsBot)
+            {
+                await RespondAsync($"Sorry, you can't add quotes from bots.");
+                return;
+            }
+
             await message.AddReactionAsync(BotExtensions.QuoteBotEmote());
             var completeMessage = await Context.GetCompleteMessage(message);
 
             await _dataService.CreateQuoteRecord(new Quoted(completeMessage));
 
             var response = new StringBuilder()
-                .AppendLine($"*{completeMessage.Content}* by {completeMessage.Author.Username} quoted!");
+                .AppendLine($"{completeMessage.Content}")
+                .AppendLine($"*by {completeMessage.Author.Username} quoted!*");
 
             await RespondAsync($"{response}");
         }

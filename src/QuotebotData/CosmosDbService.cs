c9a7c91f3905c6aa3c7a23d07ec138cde77fef81
Craig Young <youngcraig@gmail.com>
2022-01-08 01:58:04 -0700

    Completely redo this. I kinda got carried away but I think it's cleaner in the end.

--- src/QuotebotData/CosmosDbService.cs
+++ src/QuotebotData/CosmosDbService.cs
@@ -1,141 +1,50 @@
 ﻿using Discord;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using Quotebot.Data.Entities;
 
 namespace Quotebot.Data
 {
     public class CosmosDbService : IDataService
     {
-        private readonly CosmosClient _cosmosClient;
         private readonly ILogger _logger;
-        private readonly JsonSerializerOptions _jsonSerializerOptions;
+        private readonly Container _container;
 
-        //// The container we will create.
-        private Container? _container;
-
-        //// The name of the database and container we will create
-        private readonly string _databaseId = "Quotebot";
-        private readonly string _containerId = "Quotes";
-        private readonly string _partitionKeyPath = "/Author/Id";
-
-        public CosmosDbService(CosmosClient cosmosClient, ILogger<CosmosDbService> logger)
+        public CosmosDbService(Container container, ILogger<CosmosDbService> logger)
         {
-            _cosmosClient = cosmosClient;
+            if(container is null)
+                throw new ArgumentNullException(nameof(container));
+
+            _container = container;
             _logger = logger;
-
-            _jsonSerializerOptions = new(JsonSerializerDefaults.General)
-            {
-                UnknownTypeHandling = JsonUnknownTypeHandling.JsonElement
-            };
         }
 
-        public async Task Initialize()
+        public async Task CreateQuoteRecord(Quoted message)
         {
-            try
+            await _container.CreateItemAsync(message, new PartitionKey(Convert.ToString(message.Id)));
+        }
+
+        public async Task<int> QuotesCountByUser(Entities.User user)
+        {
+            return await _container.GetItemLinqQueryable<Quoted>()
+                .Where(item => item.Author != null && item.Author.Id == user.Id)
+                .CountAsync();
+        }
+
+        public async IAsyncEnumerable<Quoted> FindByQuote(string messageLike)
+        {
+            using var setIterator = _container.GetItemLinqQueryable<Quoted>()
+                                 .Where(record => record.Content.Contains(messageLike))
+                                 .ToFeedIterator();
+            
+            while (setIterator.HasMoreResults)
             {
-                _logger.LogInformation("Connecting to database...");
-                Database? database = await InitializeDatabase();
-                if (database == null)
+                foreach (var item in await setIterator.ReadNextAsync())
                 {
-                    throw new ArgumentNullException(nameof(database));
+                    yield return item;
                 }
-                await DeleteExistingContainerIfDebug(database);
-                _container = await InitializeContainer(database);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical(Convert.ToString(ex));
             }
         }
-
-        private async Task<Database> InitializeDatabase()
-        {
-            _logger.LogTrace("CreateDatabaseIfNotExistsAsync");
-            DatabaseResponse databaseResponse = await _cosmosClient.CreateDatabaseIfNotExistsAsync(_databaseId);
-
-            string output = databaseResponse.StatusCode switch
-            {
-                HttpStatusCode.OK => "Database already existed...",
-                HttpStatusCode.Created => $"Database {_databaseId} was created successfully",
-                _ => $"An error occured when attempting to create database. Http Status was {databaseResponse.StatusCode}"
-            };
-
-            _logger.LogDebug(output);
-
-            // The response from Azure Cosmos
-            DatabaseProperties properties = databaseResponse;
-
-            _logger.LogDebug($"Database resource id: {properties.Id} and last modified: {properties.LastModified}");
-
-            return databaseResponse;
-        }
-        
-        private async Task DeleteExistingContainerIfDebug(Database database)
-        {
-#if DEBUG
-            // Delete the existing container to prevent create item conflicts
-            using (await database.GetContainer(_containerId).DeleteContainerStreamAsync())
-            { }
-#else
-            await Task.CompletedTask;
-#endif
-        }
-
-        private async Task<Container> InitializeContainer(Database database)
-        {
-            _logger.LogTrace("CreateContainerIfNotExistsAsync");
-            // Set throughput to the minimum value of 400 RU/s
-            ContainerResponse containerResponse = await database.CreateContainerIfNotExistsAsync(
-                id: _containerId,
-                partitionKeyPath: _partitionKeyPath,
-                throughput: 400);
-
-            string output = containerResponse.StatusCode switch
-            {
-                HttpStatusCode.OK => "Container already existed...",
-                HttpStatusCode.Created => $"Container {_containerId} was created successfully",
-                _ => $"An error occured when attempting to create database. Http Status was {containerResponse.StatusCode}"
-            };
-
-            _logger.LogDebug(output);
-
-            ContainerProperties properties = containerResponse;
-            _logger.LogDebug($"Container resource id: {properties.Id} and last modified: {properties.LastModified}");
-
-            return containerResponse;
-        }
-
-
-
-        public async Task CreateQuoteRecord(IMessage message)
-        {
-            try
-            {
-                QuotedCosmosRecord record = new(message);
-                using var payloadStream = CosmosDbService.ToStream<QuotedCosmosRecord>(record);
-                var response = await _container!.CreateItemAsync(record, new PartitionKey(message.Author.Id));
-
-                _logger.LogDebug($"Item created");
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
-        }
-
-
-        private static Stream ToStream<T>(T input)
-        {
-            MemoryStream streamPayload = new MemoryStream();
-            JsonSerializer.Serialize(streamPayload, input);
-
-            streamPayload.Position = 0;
-            return streamPayload;
-        }
-
     }
 }

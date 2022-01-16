79b7fc2e7a7bcf3669cbb69f11c95968f83185de
Craig Young <youngcraig@gmail.com>
2022-01-16 15:45:12 -0700

    Remove this as it's not longer needed and replaced by concrete configuration implementation

--- src/Quotebot.Data/ConfigurationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Quotebot.Data
-{
-    internal static class ConfigurationConstants
-    {
-        public const string CosmosUrlConfigurationKey = "CosmosEndpointUrl";
-        public const string CosmosAuthorizationConfigurationKey = "CosmosAuthorization";
-    }
-}

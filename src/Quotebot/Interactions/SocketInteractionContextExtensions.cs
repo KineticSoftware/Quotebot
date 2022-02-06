44926e5b4b903058b6a7875a50cbb93bd8953789
Craig Young <youngcraig@gmail.com>
2022-02-05 22:12:59 -0700

    removed unused code

--- src/Quotebot/Interactions/SocketInteractionContextExtensions.cs
+++ src/Quotebot/Interactions/SocketInteractionContextExtensions.cs
@@ -8,15 +8,6 @@ public static class SocketInteractionContextExtensions
     public static async Task<IMessage> GetCompleteMessage(this SocketInteractionContext context, IMessage message) =>
         await context.Channel.GetMessageAsync(message.Id);
 
-    public static async Task<IMessage?>? GetLatestBotMessageAsync(this SocketInteractionContext context, ulong botId = 925598461695524964) =>
-        (await context.GetLatestChannelMessagesAsync())
-            .Where(message => message?.Author?.IsBot ?? false && message?.Author?.Id == botId)
-            .OrderByDescending(message => message?.CreatedAt)
-            .FirstOrDefault();
-
-    private static async Task<IEnumerable<IMessage>> GetLatestChannelMessagesAsync(this SocketInteractionContext context, int count = 50) =>
-        await context.Channel.GetMessagesAsync(count).FlattenAsync();
-
     public static async Task<User> GetGuildUserName(this SocketInteractionContext context, IUser discordUser)
     {
         await context.Guild.DownloadUsersAsync();

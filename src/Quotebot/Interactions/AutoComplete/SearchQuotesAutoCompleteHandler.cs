c7482a6f6a2c79201a687c5a84d6d9321fe293fa
Craig Young <youngcraig@gmail.com>
2022-02-05 23:10:57 -0700

    Added autocomplete handler and beta auto search slash command

---
+++ src/Quotebot/Interactions/AutoComplete/SearchQuotesAutoCompleteHandler.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Interactions;
+using System.Text.Json;
+
+namespace Quotebot.Interactions.AutoComplete;
+
+class SearchQuotesAutoCompleteHandler : AutocompleteHandler
+{
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction,
+        IParameterInfo parameter, IServiceProvider services)
+    {
+        if (autocompleteInteraction is not SocketAutocompleteInteraction interaction)
+            return await Task.FromResult(AutocompletionResult.FromError(new ArgumentException("Not a SocketAutocompleteInteraction")));
+
+        var query = (string)interaction.Data.Current.Value;
+        var channel = interaction.Channel.Name;
+        ulong userId = Convert.ToUInt64(interaction.Data.Options.FirstOrDefault(x => x.Name == "user")?.Value);
+        
+        var user = await context.Guild.GetUserAsync(userId);
+
+        var dataService = services.GetRequiredService<IDataService>();
+
+
+        List<AutocompleteResult> suggestions = new();
+        var rows = await dataService.FindQuotesByUserInChannel(user, channel, query);
+        foreach (var row in rows)
+        {
+            suggestions.Add(new AutocompleteResult(row.CleanContent, row.Content));
+        }
+
+        return await Task.FromResult(AutocompletionResult.FromSuccess(suggestions));
+    }
+}
--- src/Quotebot/Interactions/SlashCommandsModule.cs
+++ src/Quotebot/Interactions/SlashCommandsModule.cs
@@ -1,4 +1,6 @@
 ﻿using Discord.Interactions;
+using Quotebot.Interactions.AutoComplete;
+using System.Text.Json;
 
 namespace Quotebot.Interactions;
 
@@ -29,5 +31,30 @@ public class SlashCommandsModule : InteractionModuleBase<SocketInteractionContex
         var results = await _dataService.FindByQuoteInServer(text, limit);
 
         await FollowupAsync(results);
+    }
+
+    
+    [SlashCommand("find-quote-beta", "Search for a specific quote by user")]
+    public async Task FindQuoteBeta(
+        [Summary("user", "The user to search")]
+         IUser user,
+        [Summary("query", "The query for the item to search"),
+         Autocomplete(typeof(SearchQuotesAutoCompleteHandler))]
+        string content)
+    {
+        await ReplyAsync(content);
+        //await DeferAsync();
+
+        //var (success, quoted) = await _dataService.FindById(messageId);
+
+        //if (success)
+        //{
+        //    await FollowupAsync($"{quoted.CreatedAt.ToString("d")} - **{quoted.Author.Nickname ?? quoted.Author.Username}** : {quoted.Content}");
+        //}
+        //else
+        //{
+        //    await FollowupAsync($"Messaage Id {messageId} was not found");
+        //}
+
     }
 }

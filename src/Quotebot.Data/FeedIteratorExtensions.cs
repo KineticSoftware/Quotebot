d7a76c23b7f83847ab83f6603bfc3ccde7948558
Craig Young <youngcraig@gmail.com>
2022-09-01 18:30:30 -0600

    This might get thrown out. Or I might end up using it.

---
+++ src/Quotebot.Data/FeedIteratorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace Quotebot.Data
+{
+    public static class FeedIteratorExtensions
+    {
+        public static async IAsyncEnumerable<TModel> ToAsyncEnumerable<TModel>(this FeedIterator<TModel> iterator)
+        {
+            while (iterator.HasMoreResults)
+            {
+                foreach (TModel record in await iterator.ReadNextAsync())
+                {
+                    yield return record;
+                }
+            }
+        }
+    }
+}

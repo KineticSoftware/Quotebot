7d78d3acdb65df2a677ed4f930e33d016df27913
Craig Young <youngcraig@gmail.com>
2022-10-04 22:13:13 -0600

    Renamed object

--- src/Quotebot/Configuration/BotConfiguration.cs
+++ src/Quotebot/Configuration/BotConfiguration.cs
@@ -1,10 +1,10 @@
 ﻿namespace Quotebot.Configuration;
 
-internal class BotConfiguration
+internal class DiscordConfiguration
 {
     public const string ConfigurationSectionName = "Discord";
 
-    public string Token { get; set; } = string.Empty;
-    public ulong GuildId { get; set; }
+    public string Token { get; init; } = string.Empty;
+    public ulong GuildId { get; init; }
     public ulong GeneralChannelId { get; init; }
 }

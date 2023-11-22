c6ba8e15a0e25e2123332737000ddd6d68323ce4
Craig Young <youngcraig@gmail.com>
2023-11-21 22:40:40 -0700

    Added ChannelTests and SourceMock source generator mocker

---
+++ test/Quotebot.Domain.Test/ChannelTests.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Mocks;
+using Quotebot.Domain.Entities;
+
+namespace Quotebot.Domain.Test;
+
+public class ChannelTests
+{
+    [Fact]
+    public void Channel_Ctor_With_Parameters_Should_Assign_Properties()
+    {
+        ChannelMock mock = new();
+        string expectedName = "test_user";
+        mock.Setup.Name.Returns(expectedName);
+        
+        DateTimeOffset expectDateTimeOffset = new(DateTime.Now.Date);
+        mock.Setup.CreatedAt.Returns(expectDateTimeOffset);
+
+        ulong expectedChannelId = 1337;
+        mock.Setup.Id.Returns(expectedChannelId);
+
+        Channel test = new(mock);
+
+        Assert.Equal(expectedName, test.Name);
+        Assert.Equal(expectDateTimeOffset, test.CreatedAt);
+        Assert.Equal(expectedChannelId, test.Id);
+    }
+
+}
--- test/Quotebot.Domain.Test/GlobalUsings.cs
+++ test/Quotebot.Domain.Test/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using Xunit;
+using Discord;
+using SourceMock;
+
+[assembly: GenerateMocksForAssemblyOf(typeof(IChannel))]

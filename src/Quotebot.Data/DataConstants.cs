75a6745da03045b056d77a86c08c6281b7b57eb1
Craig Young <youngcraig@gmail.com>
2023-11-25 23:00:55 -0700

    Make static because this class shouldn't be newable

--- src/Quotebot.Data/DataConstants.cs
+++ src/Quotebot.Data/DataConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Quotebot.Data;
 
-internal class DataConstants
+internal static class DataConstants
 {
     public const string DatabaseId = "QuoteMage";
     public const string ContainerId = "Quotes";
